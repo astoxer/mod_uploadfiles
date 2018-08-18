0f1c2b0d7fc1723a7014884fe54376f7f5449bbc
astoxer <42495872+astoxer@users.noreply.github.com>
2018-08-19 01:19:23 +0600

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,7 +12,9 @@ defined('_JEXEC') or die; ?>
         		
 		<form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>" enctype="multipart/form-data">
             <label for="userfile">Загрузить фото</label>
-            <input type="file" id="inputfile" name="inputfile"></br>
+            <input type="hidden" name="MAX_FILE_SIZE" value="4194304"/>
+			<input type="file" id="inputfile" name="inputfile">
+			</br>
             <input type="submit" value="Налить">
         </form>
     </body>
